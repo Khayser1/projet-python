6dbd751e8a96316470f7cb6973a894713a279233
fouko36 <165049078+fouko36@users.noreply.github.com>
2024-05-06 10:35:58 +0200

    Delete class_arche.py

--- class_arche.py
+++
@@ -1,8 +0,0 @@
-import pygame as pg
-class Arche:
-    def __init__(self, points):
-        self.points = points
-        self.pieces = []
-arche_point = [(0,0),(100,0),(150,50),(100,100),(0,100)]
-BLUE = [0,0,255]
-pg.draw.polygon(BLUE, arche_point)
