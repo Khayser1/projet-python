de6dce97e99b8460d5b43e4cb023165d31308c7c
fouko36 <165049078+fouko36@users.noreply.github.com>
2024-05-06 10:34:54 +0200

    Add files via upload

---
+++ Jeux.py
@@ -0,0 +1,15 @@
+import pygame
+
+
+ 
+
+def barre_droite(surface):
+    bar_couleur=(125, 125, 113)
+    bar_position=(1080,0,200,680)
+    pygame.draw.rect(surface, bar_couleur, bar_position)
+        
+def barre_gauche(surface):
+    bar_couleur = (125, 125, 113)   
+    bar_position = (0, 0, 200, 680)     
+    pygame.draw.rect(surface, bar_couleur, bar_position)
+
